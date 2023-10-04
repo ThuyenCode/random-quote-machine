286d31e390c03ada11b6b9d0a0e82fd47d82552f
thuyencode <thuyencode@proton.me>
2023-10-04 15:46:17 +0700

    💄 Add margin

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,10 @@ import { Controls } from './components/Controls'
 
 export default function App() {
   return (
-    <article className='max-w-lg space-y-5 rounded bg-white p-4' id='quote-box'>
+    <article
+      className='mx-2 max-w-lg space-y-5 rounded bg-white p-4'
+      id='quote-box'
+    >
       <Blockquote />
       <Controls />
     </article>
